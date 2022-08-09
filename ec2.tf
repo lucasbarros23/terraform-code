c214dc4c0fc12503e402d1f328738950ca7ac2f4
lucasbarros23 <70347936+lucasbarros23@users.noreply.github.com>
2022-08-09 15:02:00 -0300

    Update and rename ec2-s3.tf to ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -34,14 +34,3 @@ resource "aws_instance" "web" {
 }
 
 
-
-#Provisionar S3
-resource "aws_s3_bucket" "b" {
-  bucket = "INFORME-UM-NOME-PARA-BUCKET"
-  acl    = "private"
-
-  tags = {
-    Name        = "INFORME-UMA-TAG-PARA-BUCKET
-    Environment = "Dev"
-  }
-}
